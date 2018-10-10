75933984ecbec96df8453f110779582da6567562
koofrank <koofranker@gmail.com>
2018-10-10 11:46:18 +0800

    add project

---
+++ Gemma-Common-Module/Gemma_Common_Module.h
@@ -0,0 +1,19 @@
+//
+//  Gemma_Common_Module.h
+//  Gemma-Common-Module
+//
+//  Created by koofrank on 2018/10/10.
+//  Copyright © 2018 com.nbltrustdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gemma_Common_Module.
+FOUNDATION_EXPORT double Gemma_Common_ModuleVersionNumber;
+
+//! Project version string for Gemma_Common_Module.
+FOUNDATION_EXPORT const unsigned char Gemma_Common_ModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gemma_Common_Module/PublicHeader.h>
+
+
